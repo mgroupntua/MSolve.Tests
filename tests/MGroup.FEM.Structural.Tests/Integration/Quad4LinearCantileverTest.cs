61cb44f5f73988df2c85eeba9519afe642fdf795
Pavlos Gkesos <gessos.paul@gmail.com>
2024-01-24 20:39:05 +0200

    Include a candilever solve with block pcg

--- tests/MGroup.FEM.Structural.Tests/Integration/Quad4LinearCantileverTest.cs
+++ tests/MGroup.FEM.Structural.Tests/Integration/Quad4LinearCantileverTest.cs
@@ -17,15 +17,15 @@ namespace MGroup.FEM.Structural.Tests.Integration
 	{
 		private static List<(INode node, IDofType dof)> watchDofs = new List<(INode node, IDofType dof)>();
 
-		[Fact]
-		private static void RunTest()
+        [Fact]
+		private static void RunPcgTest()
 		{
 			var model = Quad4LinearCantileverExample.CreateModel();
-			var log = SolveModel(model);
+			var log = SolvePcgModel(model);
 			Assert.Equal(expected: Quad4LinearCantileverExample.expected_solution_node3_TranslationX, actual: log.DOFValues[watchDofs[0].node, watchDofs[0].dof], precision: 8);
 		}
 
-		private static DOFSLog SolveModel(Model model)
+		private static DOFSLog SolvePcgModel(Model model)
 		{
 			var pcgBuilder = new PcgAlgorithm.Builder
 			{
@@ -51,5 +51,40 @@ namespace MGroup.FEM.Structural.Tests.Integration
 
 			return (DOFSLog)linearnalyzer.Logs[0];
 		}
-	}
+
+        [Fact]
+        private static void RunBlockPcgTest()
+        {
+            var model = Quad4LinearCantileverExample.CreateModel();
+            var log = SolveBlockPcgModel(model);
+            Assert.Equal(expected: Quad4LinearCantileverExample.expected_solution_node3_TranslationX, actual: log.DOFValues[watchDofs[0].node, watchDofs[0].dof], precision: 8);
+        }
+
+        private static DOFSLog SolveBlockPcgModel(Model model)
+        {
+            var pcgBuilder = new BlockPcgAlgorithm.Builder
+            {
+                ResidualTolerance = 1E-6,
+                MaxIterationsProvider = new PercentageMaxIterationsProvider(maxIterationsOverMatrixOrder: 0.5)
+            };
+
+            var solverFactory = new BlockPcgSolver.Factory();
+            solverFactory.BlockPcgAlgorithm = pcgBuilder.Build();
+            var algebraicModel = solverFactory.BuildAlgebraicModel(model);
+            var solver = solverFactory.BuildSolver(algebraicModel);
+            var problem = new ProblemStructural(model, algebraicModel);
+
+            var linearnalyzer = new LinearAnalyzer(algebraicModel, solver, problem);
+            var staticAnalyzer = new StaticAnalyzer(algebraicModel, problem, linearnalyzer);
+
+            watchDofs.Add((model.NodesDictionary[3], StructuralDof.TranslationX));
+
+            linearnalyzer.LogFactory = new LinearAnalyzerLogFactory(watchDofs, algebraicModel);
+
+            staticAnalyzer.Initialize();
+            staticAnalyzer.Solve();
+
+            return (DOFSLog)linearnalyzer.Logs[0];
+        }
+    }
 }
